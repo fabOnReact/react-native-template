b95be0cc980ee0a8846f1a696adadcf04b9b0cc9
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2021-09-22 12:22:40 +0800

    npx react-native init ProjectName --template file:///home/fabrizio/Documents/sourcecode/opensource/react-native-template/template

---
+++ ProjectName/template.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  placeholderName: "ProjectName",
+  titlePlaceholder: "ProjectName",
+  templateDir: ".",
+};
---
+++ template/template.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  placeholderName: "react-native-new-template",
+  titlePlaceholder: "react-native-new-template",
+  templateDir: ".",
+};
